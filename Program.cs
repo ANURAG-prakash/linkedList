e4e62c2e8959cc67c2024882feea0ad670617feb
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-30 07:45:20 +0530

    UC6 Added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Linked_List list = new Linked_List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.InsertedAtParticular(2, 30);
+            list.RemoveFirstNode();
+            list.RemoveLastNode();
+
+
+        }
+    }
+}
