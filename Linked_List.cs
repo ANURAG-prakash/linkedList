132b28708850b5bc040aa6837a58e49281f85a72
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-30 07:30:51 +0530

    UC1

---
+++ Linked_List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class Linked_List
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head==null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted Into LinkedList", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("Linked list is Empty");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
