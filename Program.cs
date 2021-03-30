7afc9af070796ae3d7112283be12d4c59ea89557
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-30 07:43:50 +0530

    Anurag Prakash has Added UC5

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Linked_List list = new Linked_List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.insertedAtParticular(2, 30);
+            list.RemoveFirstNode();
+
+
+        }
+    }
+}
